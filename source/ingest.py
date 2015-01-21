c7f638851072a080302be918bbcecde7f3245e96
julbright <jim.ulbright@gmail.com>
2015-01-20 22:45:14 -0500

    ingest script now gets some of the series data off of bls that might be useful. can theoretically add more urls to download by adding to the urls.txt file located in source, but its not that robust. so it will probably break at this point

---
+++ source/ingest.py
@@ -0,0 +1,55 @@
+import requests
+import os
+import csv
+
+###############################################################################
+# This module gets all of the data from urls listed in urls.txt
+# and exports them as txt files in the /fixtures dir.
+#
+# Some of this isn't data--it's supporting documentation that
+# explains what the data is, etc. Load them judiciously! 
+###############################################################################
+
+
+
+
+def get_urls(url_file = './urls.txt'):
+	"""
+	Right now, simply returns a dictionary of predefined
+	urls. Later, might load from a seperate resource
+
+	Returns: dictionary of {"description" : "url"} pairs
+	"""
+
+	urls = dict()
+
+	with open(url_file, 'rb') as f:
+		for each_item in f.read().split(','):
+			key   = each_item.split(' : ')[0].strip()
+			value = each_item.split(' : ')[1].strip()
+			urls[key] = value
+
+	return urls
+
+
+def get_data(urls, export_dir = os.path.realpath(__file__)):
+	"""
+	Given a dictionary where the values are urls,
+	gets the resource listed at each url and dumps it 
+	into a directory of the user's chosing.
+	"""
+	
+	for url in urls.keys():
+		r = requests.get(urls[url])
+		r.raise_for_status()
+		print 'writing to {0}.txt'.format(url)
+		with open(export_dir+"{0}.txt".format(url), 'wb') as f:
+			f.write(r.text)
+
+def main():
+	urls = get_urls()
+	get_data(urls, export_dir = '../fixtures/')
+
+
+if __name__ == '__main__':
+	main()
